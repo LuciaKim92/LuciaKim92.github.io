7d8b24a7135d9f8fc7327366a127fd4d279ea734
kojihun <jihun.ko@computer.co.kr>
2021-10-05 13:05:14 +0900

    sprint 미팅 db 연동

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 use App\Models\DashBoardModel;
 use App\Models\LayoutModel;
+use App\Models\Sprint_Meet_Model;
 
 class Home extends BaseController
 {
@@ -119,57 +120,20 @@ class Home extends BaseController
 	// sprint meeting 회의록 작성
 	public function sprint()
 	{
-		//1. 저번주에 회의록에 작성한 다음주 할 일 부분 (피드백 부분에서 불러올거임)
-		$last_week_plan1 = array("kr" => "1", "담당자" => "고지훈", "할일" => "개발하기1");
-		$last_week_plan2 = array("kr" => "1", "담당자" => "이유진", "할일" => "개발하기2");
-		$last_week_plan3 = array("kr" => "2", "담당자" => "김경민", "할일" => "개발하기3");
-		$last_week_plan4 = array("kr" => "2", "담당자" => "고지훈", "할일" => "개발하기4");
-		$last_week_plan5 = array("kr" => "3", "담당자" => "이유진", "할일" => "개발하기5");
-		$last_week_plan6 = array("kr" => "3", "담당자" => "김경민", "할일" => "개발하기6");
-		$last_week_plan7 = array("kr" => "4", "담당자" => "고지훈", "할일" => "개발하기7");
-		$last_week_plan8 = array("kr" => "4", "담당자" => "이유진", "할일" => "개발하기8");
-		$last_week_plan9 = array("kr" => "5", "담당자" => "김경민", "할일" => "개발하기9");
-		$last_week_plan10 = array("kr" => "5", "담당자" => "고지훈", "할일" => "개발하기10");
-		
-		//2. KR에 따른 데이터 모음집 (피드백 부분)
-		$kr1_last_week_plan = array("1"=>$last_week_plan1, "2"=>$last_week_plan2);
-		$kr2_last_week_plan = array("1"=>$last_week_plan3, "2"=>$last_week_plan4);
-		$kr3_last_week_plan = array("1"=>$last_week_plan5, "2"=>$last_week_plan6);
-		$kr4_last_week_plan = array("1"=>$last_week_plan7, "2"=>$last_week_plan8);
-		$kr5_last_week_plan = array("1"=>$last_week_plan9, "2"=>$last_week_plan10);
+		$this->Model1 = new Sprint_Meet_Model;
 
-		//3. 피드백 부분 데이터
-		$feedback = array(
-			"1"=> $kr1_last_week_plan,
-			"2"=> $kr2_last_week_plan,
-			"3"=> $kr3_last_week_plan,
-			// "4"=> $kr4_last_week_plan,
-			// "5"=> $kr5_last_week_plan,
-		);
+		$myarr = $this->Model1->search_spr_by_id(1000000);
 
-		
-		// KEY RESULT 배열
-		$kr = array(
-			"1" => "나는 kr 1 입니다",
-			"2" => "나는 kr 2 입니다",
-			"3" => "나는 kr 3 입니다",
-			// "4" => "나는 kr 4 입니다",
-			// "5" => "나는 kr 5 입니다",
-		);
+		echo $myarr['OBJECTIVE'];
+		$myarr['MEET_DT'] = date("Y-m-d", strtotime($myarr['MEET_DT']));
+        echo $myarr['DEPT_NM'];
+        echo $myarr['DEPT_UP_NM'];
+        foreach($myarr['KR'] as $key => $bean){
+            echo $bean['ID'];
+			echo $bean['CONTENT'];
+        }
 
-
-
-		//담당자 배열
-		$manager = array(
-			["이름" => "고지훈", "사원번호" => "111"],
-			["이름" => "이유진", "사원번호" => "222"],
-			["이름" => "김경민", "사원번호" => "333"],
-		);
-
-
-		$mydata['myarray'] = $kr;
-		$mydata['feedback'] = $feedback;
-		$mydata['manager'] = $manager;
+		$mydata['myarr'] = $myarr;
 
 		$this->session_setting();
 		return view('sprint/sprint.php', $mydata);
@@ -177,67 +141,13 @@ class Home extends BaseController
 	}
 
 	// sprint meeting 회의록 보여주는거
-	public function sprint2(){
+	public function sprint2($id)
+	{
+		$this->Model1 = new Sprint_Meet_Model;
 
-		//feedback 데이터
-		$feedback_list_1 = array(
-			["담당자" => "고지훈", "한일" => "스프린트 회의록 양식 개발", "high" => "조금 손보면 될것 같네!", "low" => "잘했네"],
-			["담당자" => "이유진", "한일" => "데쉬보드 UI 개발", "high" => "이 부분 조금만 수정하게!", "low" => "잘했어"],
-			["담당자" => "김경민", "한일" => "스프린트 회의록 목록 개발", "high" => "좀더 해보게!", "low" => "잘했어 잘했어"],
-		);
-
-		$feedback_list_2 = array(
-			["담당자" => "고지훈2", "한일" => "스프린트 회의록 양식 개발2", "high" => "조금 손보면 될것 같네!", "low" => "잘했네"],
-			["담당자" => "이유진2", "한일" => "데쉬보드 UI 개발2", "high" => "이 부분 조금만 수정하게!", "low" => "잘했어"],
-		);
-
-		$feedback_list_3 = array(
-			["담당자" => "김경민3", "한일" => "스프린트 회의록 목록 개발3", "high" => "좀더 해보게!", "low" => "잘했어 잘했어"],
-		);
-
-		//idea 데이터
-		$idea_list_1 = array(
-			["담당자" => "고지훈", "전략" => "밤 세워서 개발하기!", "아이디어" => "카페인을 섭취하기"],
-		);
-
-		$idea_list_2 = array(
-			["담당자" => "고지훈", "전략" => "오류없는 개발하기!", "아이디어" => "인터넷 찾아보기"],
-			["담당자" => "이유진", "전략" => "즐겁게 개발하기!", "아이디어" => "충분한 휴식시간 갖기"],
-		);
-
-		$idea_list_3 = array(
-			["담당자" => "고지훈", "전략" => "밤 세워서 개발하기!", "아이디어" => "카페인을 섭취하기"],
-			["담당자" => "이유진", "전략" => "밤 세워서 개발하기!", "아이디어" => "카페인을 섭취하기"],
-			["담당자" => "김경민", "전략" => "밤 세워서 개발하기!", "아이디어" => "카페인을 섭취하기"],
-		);
-
-		//plan 데이터
-		$plan_list_1 = array(
-			["담당자" => "고지훈", "할일" => "스프린트 미팅 회의록 화면 구현하기"],
-			["담당자" => "이유진", "할일" => "데쉬보드 화면 구현하기"],
-			["담당자" => "김경민", "할일" => "스프린트 미팅 목록 구현하기"],
-		);
-
-		$plan_list_2 = array(
-			["담당자" => "고지훈2", "할일" => "스프린트 미팅 회의록 화면 구현하기2"],
-			["담당자" => "이유진2", "할일" => "데쉬보드 화면 구현하기2"],
-			["담당자" => "김경민2", "할일" => "스프린트 미팅 목록 구현하기2"],
-		);
-
-		$plan_list_3 = array(
-			["담당자" => "고지훈3", "할일" => "스프린트 미팅 회의록 화면 구현하기3"],
-			["담당자" => "이유진3", "할일" => "데쉬보드 화면 구현하기3"],
-			["담당자" => "김경민3", "할일" => "스프린트 미팅 목록 구현하기3"],
-		);
-
-		//KEY RESULT 배열 3개만 있다고 가정
-		$kr_list = array(
-			["index" => "1", "content" => "kr1 입니다.", "feedback" => $feedback_list_1, "idea" => $idea_list_1, "plan" => $plan_list_1],
-			["index" => "2", "content" => "kr2 입니다.", "feedback" => $feedback_list_2, "idea" => $idea_list_2, "plan" => $plan_list_2],
-			["index" => "3", "content" => "kr3 입니다.", "feedback" => $feedback_list_3, "idea" => $idea_list_3, "plan" => $plan_list_3]
-		);
-
-		$mydata['myarray'] = $kr_list;
+		$myarr = $this->Model1->search_spr_by_id($id);
+		$myarr['MEET_DT'] = date("Y-m-d", strtotime($myarr['MEET_DT']));
+		$mydata['myarr'] = $myarr;
 
 		$this->session_setting();
 		$data = $_SESSION;
---
+++ app/Models/Sprint_Meet_Model.php
@@ -0,0 +1,173 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Sprint_Meet_Model extends Model
+{
+    public function __construct(){
+        $this->serverName = "211.233.21.82";
+        $this->connectionOptions = array(
+            "database" => "", // 데이터베이스명
+            "uid" => "dwokr",   // 유저 아이디
+            "pwd" => "dwokr@)@!21"    // 유저 비번
+        );
+
+
+        $this->dbconn = sqlsrv_connect($this->serverName, $this->connectionOptions); 
+    }
+
+    public function search_spr_by_date($date){
+        $result = array();
+
+        $query = "SELECT * from SPR_MEET_MST WHERE MEET_DT = $date";
+        $stmt = sqlsrv_query($this->dbconn, $query);
+        $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+
+        if($row == NULL)
+            return null;
+            
+        return $row['ID'];
+    }
+
+    public function search_spr_by_id($id){
+
+        //최종적으로 리턴
+        $result = array();
+
+        //SPR_MEET_MST랑 부서 테이블을 조인하여 다른 테이블 조회에 사용될 데이터 가져옴(결과 1개만 나옴...)
+        $query = "  SELECT A.OKR_OBJT_ID, A.MEET_DT, A.DEPT_NM, B.DEPT_NM as 'DEPT_UP_NM'
+                    FROM	(	SELECT A.OKR_OBJT_ID, A.MEET_DT, B.DEPT_NM, B.DEPT_UP_CD 
+                                FROM SPR_MEET_MST A INNER JOIN DWCTS.dbo.DEPT_MST B ON A.DWGP_CD = B.DWGP_CD AND A.DEPT_CD = B.DEPT_CD 
+                                WHERE A.ID = $id) A INNER JOIN DWCTS.dbo.DEPT_MST B ON A.DEPT_UP_CD = B.DEPT_CD";
+
+        $stmt = sqlsrv_query($this->dbconn, $query);
+        $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+
+
+        //예외처리 해야함 해당 회의록이 없거나, 부서코드가 잘못됐거나...
+        if($row == NULL){
+            echo "오류";
+        }
+
+        //오브젝티브 아이디 및 이름
+        $OBJT_ID = $row['OKR_OBJT_ID'];
+        $result['MEET_DT'] = $row['MEET_DT'];
+        $result['DEPT_NM'] = $row['DEPT_NM'];
+        $result['DEPT_UP_NM'] = $row['DEPT_UP_NM'];
+
+        $query = " SELECT OBJECTIVE FROM OKR_OBJT_MST WHERE ID = $OBJT_ID";
+        $stmt = sqlsrv_query($this->dbconn, $query);
+        $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+
+        $result['OBJECTIVE'] = $row['OBJECTIVE'];
+
+
+        //KR 조회
+        $query = "SELECT ID, CONTENT from OKR_KEYS_MST WHERE OKR_OBJT_ID = '$OBJT_ID' AND PROC_ST NOT IN ('8', '9')";
+        $stmt = sqlsrv_query($this->dbconn, $query);
+
+        $kr_arr = array();
+
+        while ($row2 = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+        {
+            array_push($kr_arr, ['ID' => $row2['ID'], 'CONTENT' => $row2['CONTENT']]);
+        }
+
+        $result['KR'] = $kr_arr;
+
+
+        //KR 별로 피드백 조회
+
+        $feed_arr = array();
+
+        foreach($kr_arr as $key => $bean){
+            // echo $key;
+            $kr_id = $bean['ID'];
+
+            $query = "  SELECT A.HIGHLIGHT, A.LOWLIGHT, B.EMP_NM, B.CONTENT 
+                        FROM SPR_MEET_FEED A INNER JOIN (SELECT A.*, B.EMP_NM FROM SPR_MEET_PLAN A INNER JOIN DWCTS.dbo.EMP_MST B ON A.EMPY_NO = B.EMP_NO) B ON A.SPR_MEET_PLAN_ID = B.ID
+                        WHERE OKR_KEYS_ID = $kr_id AND A.SPR_MEET_ID = $id";
+            $stmt = sqlsrv_query($this->dbconn, $query);
+
+            $temp = [];
+            
+            while ($row3 = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+            {
+                array_push($temp, $row3);
+            }
+
+            array_push($feed_arr, $temp);
+        }
+
+        $result['FEED'] = $feed_arr;
+
+
+        //KR 별로 아이디어 조회
+
+        $idea_arr = array();
+
+        foreach($kr_arr as $key => $bean){
+            // echo $key;
+            $kr_id = $bean['ID'];
+
+            $query = "  SELECT A.CONTENT as 'IDEA_CONTENT', A.EMP_NM, B.CONTENT as 'TO_DO_CONTENT', B.INIT_CONTENT
+            FROM    (SELECT A.*, B.EMP_NM FROM SPR_MEET_IDEA A INNER JOIN DWCTS.dbo.EMP_MST B ON A.EMPY_NO = B.EMP_NO) A INNER JOIN 
+                    (SELECT A.*, B.CONTENT INIT_CONTENT FROM OKR_TODO_MST A INNER JOIN OKR_INIT_MST B ON A.OKR_INIT_ID = B.ID) B ON A.OKR_TODO_ID = B.ID
+            WHERE A.SPR_MEET_ID = $id AND A.OKR_KEYS_ID = $kr_id";
+
+            $stmt = sqlsrv_query($this->dbconn, $query);
+
+            $temp = [];
+            while ($row3 = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+            {
+                array_push($temp, $row3);
+            }
+
+            array_push($idea_arr, $temp);
+        }
+
+        $result['IDEA'] = $idea_arr;
+
+
+        //KR 별로 플랜 조회
+
+        $plan_arr = array();
+
+        
+        foreach($kr_arr as $key => $bean){
+            // echo $key;
+            $kr_id = $bean['ID'];
+
+            $query = "  SELECT B.EMP_NM, A.CONTENT 
+                        FROM SPR_MEET_PLAN A INNER JOIN DWCTS.dbo.EMP_MST B ON A.EMPY_NO = B.EMP_NO
+                         WHERE A.SPR_MEET_ID = $id AND A.OKR_KEYS_ID=$kr_id ";
+            
+
+            $stmt = sqlsrv_query($this->dbconn, $query);
+
+            $temp = [];
+            while ($row3 = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+            {
+                array_push($temp, $row3);
+            }
+
+            array_push($plan_arr, $temp);
+        }
+
+        $result['PLAN'] = $plan_arr;
+
+
+
+        // result의 KEY: (MEET_DT, DEPT_NM, DEPT_UP_NM, KR, FEED, IDEA, PLAN)
+        // KR의     KEY: (ID, CONTENT)
+        // FEED의   KEY: (HIGHLIGHT, LOWLIGHT, EMP_NM, CONTENT(저번주 plan) )
+        // IDEA의   KEY: (IDEA_CONTENT, EMP_NM, TO_DO_CONTENT, INIT_CONTENT)
+        // PLAN의   KEY: (EMP_NM, CONTENT )
+        return $result;
+
+    }
+
+
+
+}
