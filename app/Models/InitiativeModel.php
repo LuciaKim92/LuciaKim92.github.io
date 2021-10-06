cc6568a1b1b26ac7497dc6a7130104127c702dda
LeeYouJin <youjin.lee@computer.co.kr>
2021-10-06 15:32:27 +0900

    dashboard 95%

---
+++ app/Models/InitiativeModel.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Database\ConnectionInterface;
+use CodeIgniter\Model;
+
+class LayoutModel extends Model{
+    var $admin_db;
+	var $admin_db_cfg;
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function GetObjective($dept_CD)
+    {
+        //$this->db = \Config\Database::connect();
+        $okr_db = \Config\Database::connect('okrdb');
+        //$cts_db = \Config\Database::connect('ctsdb');
+
+        $builder = $okr_db->table('OKR_OBJT_MST');
+        $builder->select('*');
+        $builder->where('DEPT_CD', $dept_CD);
+        $query  = $builder->get();
+        return $query->getRowArray();
+        //새로운 소스 끝
+        //ci4 db by lcs 210405 끝
+    }
+
+    public function GetNavBarData()
+    {
+        //$this->db = \Config\Database::connect();
+        //$okr_db = \Config\Database::connect('okrdb');
+        $cts_db = \Config\Database::connect('ctsdb');
+
+        $builder = $cts_db->table('EMP_MST');
+        $builder->select('*');
+        $builder->where('EMP_EMAIL', 'youjin.lee@computer.co.kr');
+        $query  = $builder->get();
+
+        return $query->getRowArray();
+        //새로운 소스 끝
+        //ci4 db by lcs 210405 끝
+    }
+    
+    public function GetKeyResult($dept_CD, $dept_ST){
+        $okr_db = \Config\Database::connect('okrdb');
+        $builder = $okr_db->table('OKR_KEYS_MST');
+        $builder->select('*');
+        $builder->where('DEPT_CD', $dept_CD);
+        $builder->where('PROC_ST', $dept_ST);
+        $query  = $builder->get();
+        //print_r($query->getResult());
+        return $query->getResultArray();
+
+    }
+
+    public function GetDeptCode($dept_CD){
+        //DB 연결
+        $cts_db = \Config\Database::connect('ctsdb');
+        
+        $builder = $cts_db->table('DEPT_MAP');
+        $builder->select('*');
+        $builder->where('DEPT_CD',$dept_CD);
+        $builder->where('BEND_DT','29991231');
+        $query = $builder->get();
+        return $query->getRowArray();
+        
+    }
+    
+
+}
+
+?>
---
+++ app/Views/initiative/view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- 책갈피 Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">OKR System을 구축한다</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h5><?php print_r($id)?></h5>
+                    <p>세부내용</p>
+                    <hr>
+                    <h5>2021-09-14 (클릭시 이동)</h5>
+                    <p>세부내용</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
