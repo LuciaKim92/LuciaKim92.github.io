0b66445a9d6a55eebf160598caf50f3d47232efd
kyungmin_kim <kyungmin.kim@computer.co.kr>
2021-10-21 23:10:19 +0900

    kyungmin commit

--- app/Views/sprint/sprint_list.php
+++ app/Views/sprint/sprint_list.php
@@ -5,6 +5,7 @@
         <meta name=description content="">
         <meta name=viewport content="width=device-width, initial-scale=1">
         <title>Jquery Comments Plugin</title>
+        
         <link rel="stylesheet" type="text/css" href="/css/App.css" />
         <link rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
